3d1c941dbbe9bd4206c5946e19d612fb313ef28e
inha-kimhyunmin <kimhm396@gmail.com>
2025-08-15 22:59:55 +0900

    v2.3 대화 소리 추가

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 # 기본 오디오 설정
 SAMPLE_RATE = 16000
 SEGMENT_DURATION = 5  # 실시간 처리 단위 (초)
-DANGET_DETECT_MODEL_PATH = 'model/Danger_detect/yamnet_lstm_model_20250812_173732.h5'
+DANGET_DETECT_MODEL_PATH = 'model/Danger_detect/yamnet_lstm_model_20250815_224127.h5'
 CALIBRATION_RESULT_PATH = 'calibration_result.json'
-MIC_NUM = 2 #채널 수(시연시는 1로 조절)
+MIC_NUM = 1 #채널 수(시연시는 1로 조절)
 
 DANGER_DETECT_INPUT_DURATION = 5
 MACHINE_ANOMALY_INPUT_DURATION = 10
@@ -33,7 +33,7 @@ PART_CONFIGS = {
 }
 
 #환경 소리 판단 클래스
-CLASSES = ['Silence', 'Factory', 'Fire', 'Gas_Leak', 'Scream']
+CLASSES = ['Silence', 'Factory', 'Fire', 'Gas_Leak', 'Scream', 'Others(Talking)']
 #공장 클래스 번호
 FACTORY_CLASS = CLASSES.index('Factory')
 #공장 소리 판단 임계 확률
@@ -41,11 +41,10 @@ FACTORY_THRESHOLD = 0.6
 #위험 소리 판단 임계 확률
 DANGER_THRESHOLD = 0.7
 #판단할 기계 부품목록(시연시 이중 하나만 입력하면된다.) 각 채널별로 몇 개 만들것인가
-DETECT_PARTS = [['bearing', 'gearbox', 'fan', 'pump', 'slider'], 
-                ['bearing', 'gearbox']    
+DETECT_PARTS = [['bearing', 'gearbox', 'fan', 'pump', 'slider']
                ]
 
 #기계 소리의 일정 비율 이하 소리는 무음처리
-USE_MUTE_BY_FACTORY_SOUND = True 
+USE_MUTE_BY_FACTORY_SOUND = False 
 #기계 소리의 몇 %보다 작으면 무음이냐?
 FACTORY_SOUND_RATIO = 0.5
